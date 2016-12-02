02011a09a21a26318b575660ac84ff76b7164316
Rainer Du <dusiyh@gmail.com>
2016-12-02 23:48:00 +0800

    prepared for publish to hex

--- mix.exs
+++ mix.exs
@@ -5,9 +5,23 @@ defmodule Callbackex.Mixfile do
     [app: :callbackex,
      version: "0.1.0",
      elixir: "~> 1.3",
+     elixirc_paths: ["lib"],
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     # Docs
+     name: "Callbackex",
+     source_url: "https://github.com/secretworry/callbackex",
+     docs: [main: "Callbackex",
+            extras: ["README.md"]]]
+  end
+
+  defp description do
+      """
+      Define and execute callbacks with ease in Elixir
+      """
   end
 
   # Configuration for the OTP application
@@ -16,6 +30,17 @@ defmodule Callbackex.Mixfile do
   def application do
     [applications: [:logger]]
   end
+
+  defp package do
+    [
+      name: :callbackex,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["dusiyh@gmail.com"],
+      licenses: ["Apache 2"],
+      links: %{"GitHub" => "https://github.com/secretworry/callbackex"}
+    ]
+  end
+
 
   # Dependencies can be Hex packages:
   #
@@ -27,6 +52,6 @@ defmodule Callbackex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
